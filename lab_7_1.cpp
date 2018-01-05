7f877e928d81f49c0539cd381a3887b0543f5b74
Unknown <Arnon-1251@hotmail.com>
2018-01-05 22:14:16 +0700

    แก้ข้อ 1 เสร็จ

--- lab_7_1.cpp
+++ lab_7_1.cpp
@@ -6,13 +6,13 @@ int main() {
 
   cout << "This is simple loop"<< endl << endl ;
 
-  int i = 0;
+  int i = 1;
 
   // ศึกษาตัวอย่างการใช้ for
-  for(i = 0; i < 10 ; i++) {
-    cout << "Round " << i << endl;
+   for(i = 1; i <= 8 ; i++) {
+    cout << "Hello World " << i << endl;
   }
-printf("i = %d",i);
+
   // TODO : แก้ไขส่วนนี้ด้วยการใช้ for
   cout << "Hello World 1" << endl;
   cout << "Hello World 2" << endl;

9f26f3ae9cd0a9dade32e0ce66184ec874c92da6
Unknown <Arnon-1251@hotmail.com>
2018-01-22 17:30:29 +0700

    เสร็จแล้วจ้าาา

--- lab_7_1.cpp
+++ lab_7_1.cpp
@@ -14,14 +14,8 @@ int main() {
   }
 
   // TODO : แก้ไขส่วนนี้ด้วยการใช้ for
-  cout << "Hello World 1" << endl;
-  cout << "Hello World 2" << endl;
-  cout << "Hello World 3" << endl;
-  cout << "Hello World 4" << endl;
-  cout << "Hello World 5" << endl;
-  cout << "Hello World 6" << endl;
-  cout << "Hello World 7" << endl;
-  cout << "Hello World 8" << endl;
+
+
 
   return 0;
 }

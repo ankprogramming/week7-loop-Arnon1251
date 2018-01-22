48d857c6b65daddc37172170de769275cdc28a20
Unknown <Arnon-1251@hotmail.com>
2018-01-22 17:54:45 +0700

    เสร็จ

--- lab_7_3.cpp
+++ lab_7_3.cpp
@@ -4,20 +4,13 @@ using namespace std;
 
 int main() {
 
-  cout << "For to while" << endl;
-
-  // ตัวอย่างโปรแกรมที่ใช้ for
-
-    int i = 0;
-  while (i < 10) {
-    cout << "Run from for" << endl;
-
-
-  cout << "************END FOR************" << endl;
-  }
   // เขียนโปรแกรมที่ทำงานเหมือนด้านบนโดยใช้ while แทน for
 
-
-
+    int j = 0;
+    while (j < 10) {
+        cout << "Run from while" << endl;
+        j++;
+    }
+cout << "************END FOR************" << endl;
   return 0;
 }
